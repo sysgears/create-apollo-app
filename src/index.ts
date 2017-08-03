f87d647b5b767ce389ef484f9dab7617b109794e
Victor Vlasenko <victor.vlasenko@sysgears.com>
2017-08-03 20:37:58 +0300

    Add execute WIP impl

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import * as fs from 'fs';
 import requireModule from './requireModule';
+import execute from './executor';
 
 if (process.argv.length >= 3) {
-  const cmd = process.argv[2];
-  if (cmd === 'watch' || cmd === 'build' || cmd === 'test') {
-    let createConfig;
-    if (fs.existsSync('spin.config.js')) {
-      createConfig = requireModule('./spin.config.js').default;
-    } else {
-      createConfig = requireModule('spinjs/spin.config.js').default;
+    const cmd = process.argv[2];
+    let config;
+    if (cmd === 'watch' || cmd === 'build' || cmd === 'test') {
+        let createConfig;
+        if (fs.existsSync('spin.config.js')) {
+            createConfig = requireModule('./spin.config.js').default;
+        } else {
+            createConfig = requireModule('spinjs/spin.config.js').default;
+        }
+        config = createConfig(cmd);
     }
-    const config = createConfig(cmd);
-    console.log(`'${cmd}' config: ${config}`);
-  }
+    execute(cmd, config);
 }
