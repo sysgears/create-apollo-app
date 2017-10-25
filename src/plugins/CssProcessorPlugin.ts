0111ff782112023e1c8eeafbe86246f8904723dd
Victor Vlasenko <victor.vlasenko@sysgears.com>
2017-10-25 19:43:51 +0300

    Fix default auto post css (#20)

--- src/plugins/CssProcessorPlugin.ts
+++ src/plugins/CssProcessorPlugin.ts
@@ -3,6 +3,16 @@ import { ConfigPlugin } from '../ConfigPlugin';
 import requireModule from '../requireModule';
 import Spin from '../Spin';
 
+const postCssDefaultConfig = () => {
+  return {
+    plugins: () => [
+      requireModule('autoprefixer')({
+        browsers: ['last 2 versions', 'ie >= 9']
+      })
+    ]
+  };
+};
+
 export default class CssProcessorPlugin implements ConfigPlugin {
   public configure(builder: Builder, spin: Spin) {
     const stack = builder.stack;
@@ -12,6 +22,7 @@ export default class CssProcessorPlugin implements ConfigPlugin {
       let createRule;
       const rules = [];
       const postCssLoader = requireModule.probe('postcss-loader');
+      const useDefaultPostCss: boolean = spin.options.useDefaultPostCss || false;
       if (stack.hasAny('server')) {
         createRule = (ext, ruleList) => ({
           test: new RegExp(`\\.${ext}$`),
@@ -19,7 +30,14 @@ export default class CssProcessorPlugin implements ConfigPlugin {
             { loader: requireModule.resolve('isomorphic-style-loader') },
             { loader: requireModule.resolve('css-loader'), options: { sourceMap: true } }
           ]
-            .concat(postCssLoader ? { loader: postCssLoader, options: { sourceMap: true } } : [])
+            .concat(
+              postCssLoader
+                ? {
+                    loader: postCssLoader,
+                    options: useDefaultPostCss ? { ...postCssDefaultConfig(), sourceMap: true } : { sourceMap: true }
+                  }
+                : []
+            )
             .concat(ruleList)
         });
       } else if (stack.hasAny('web')) {
@@ -40,13 +58,34 @@ export default class CssProcessorPlugin implements ConfigPlugin {
                   { loader: requireModule.resolve('style-loader') },
                   { loader: requireModule.resolve('css-loader'), options: { sourceMap: true, importLoaders: 1 } }
                 ]
-                  .concat(postCssLoader ? { loader: postCssLoader, options: { sourceMap: true } } : [])
+                  .concat(
+                    postCssLoader
+                      ? {
+                          loader: postCssLoader,
+                          options: useDefaultPostCss
+                            ? { ...postCssDefaultConfig(), sourceMap: true }
+                            : { sourceMap: true }
+                        }
+                      : []
+                  )
                   .concat(ruleList)
               : plugin.extract({
                   fallback: requireModule.resolve('style-loader'),
-                  use: [requireModule.resolve('css-loader'), requireModule.resolve('postcss-loader')].concat(
-                    ruleList ? ruleList.map(rule => rule.loader) : []
-                  )
+                  use: [
+                    {
+                      loader: requireModule.resolve('css-loader'),
+                      options: { importLoaders: postCssLoader ? 1 : 0 }
+                    }
+                  ]
+                    .concat(
+                      postCssLoader
+                        ? {
+                            loader: postCssLoader,
+                            options: useDefaultPostCss ? postCssDefaultConfig() : {}
+                          } as any
+                        : []
+                    )
+                    .concat(ruleList ? ruleList.map(rule => rule.loader) : [])
                 })
           };
         };
