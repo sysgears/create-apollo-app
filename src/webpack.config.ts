fc307d2189366e379d6e4a5c7a063ae4cf012661
Victor Vlasenko <victor.vlasenko@sysgears.com>
2017-12-18 20:01:03 +0200

    Fix built-in webpack.config.js

--- src/webpack.config.ts
+++ src/webpack.config.ts
@@ -1,7 +1,18 @@
 import createConfig from './createConfig';
 
-const config = createConfig('test', {}).builders;
-const testConfig = config[Object.keys(config)[0]].config;
+const builders = createConfig('test', {}).builders;
+let config;
+for (const name of Object.keys(builders)) {
+  const builder = builders[name];
+  if (builder.enabled !== false && builder.roles.indexOf('test') >= 0) {
+    config = builder.config;
+    break;
+  }
+}
+
+if (!config) {
+  throw new Error('spin.js test config not found!');
+}
 
 // console.log("test config:", require('util').inspect(testConfig, false, null));
-export default testConfig;
+export default config;
