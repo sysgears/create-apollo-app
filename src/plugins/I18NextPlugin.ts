a31e9a83ce811ae2d08f96f9652fe3dd678bfe1f
Evgenij Olshanskij <gekanchiko@gmail.com>
2018-03-26 14:47:14 +0300

    Fix the loader name. Modified docs

--- src/plugins/I18NextPlugin.ts
+++ src/plugins/I18NextPlugin.ts
@@ -6,7 +6,7 @@ export default class I18NextPlugin implements ConfigPlugin {
   public configure(builder: Builder, spin: Spin) {
     const stack = builder.stack;
 
-    if (stack.hasAll(['i18n', 'webpack'])) {
+    if (stack.hasAll(['i18next', 'webpack'])) {
       const webpack = builder.require('webpack');
 
       builder.config = spin.merge(builder.config, {
