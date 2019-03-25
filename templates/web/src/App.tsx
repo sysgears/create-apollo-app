28afea86657f2da68d3cc49b4646f2de3bfbe845
Victor Vlasenko <victor.vlasenko@sysgears.com>
2019-03-25 08:17:30 +0200

    Get rid of monorepo

--- templates/web/src/App.tsx
+++ templates/web/src/App.tsx
@@ -47,7 +47,9 @@ const ServerHello = () => (
 
 const App = () => (
   <div>
-    <h1>Welcome to your own GraphQL web front end!</h1>
+    <h1>
+      Welcome to your own <a href="http://localhost:8080/graphiql">GraphQL</a> web front end!
+    </h1>
     <h2>You can start editing source code and see results immediately</h2>
     <LocalHello />
     <ServerHello />
